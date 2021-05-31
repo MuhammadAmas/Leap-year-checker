9f6f55d6054ad28da5ca841a52ee257857d12670
AmasWaseem <amaswaseem@gmail.com>
2021-05-31 12:01:55 +0500

    first commit

---
+++ leapyear.py
@@ -0,0 +1,16 @@
+x=int(input("year \n"))
+l=x % 4
+m=x % 100
+n=x % 400
+if (l == 0) :
+    if (m == 0):
+        if (n == 0):
+            print("x is a leap year")
+        else:
+            print("x is not a leap year")
+    else:
+        print("x ix a leap year")         
+else: 
+        print("x is not a leap year")    
+         
+
